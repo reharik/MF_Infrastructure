9377ab090c8442616139c16d730128431de3ca02
rhark <rharik@xogrp.com>
2015-07-13 17:29:10 -0500

    more refactoring

--- src/dependz/src/Container.js
+++ src/dependz/src/Container.js
@@ -12,27 +12,30 @@ var invariant = require('invariant');
 
 module.exports =  class Container{
     constructor(registryFuncArray){
+        //TODO CLEAN UP!!
         if(!_.isArray(registryFuncArray)){registryFuncArray = [registryFuncArray]}
 
         invariant(registryFuncArray
             && registryFuncArray[0]
             && _.isFunction(registryFuncArray[0]), 'Container requires at least one registry function');
 
-        this.registry = this.buildRegistry(registryFuncArray);
-console.log(this.registry);
-        this.dependencyGraph = new Graph();
+        this.registryFunkArray = registryFuncArray;
+        this.registry = this.buildRegistry();
+        this.dependencyGraph = new Graph(this.registry.appRoot);
         var packageJson =  require(this.registry.pathToPackageJson);
         this.dependencyGraph.buildGraph(packageJson);
         applyRegistry(this.registry, this.dependencyGraph);
         new GraphResolution().recurse(this.dependencyGraph);
     }
 
-    buildRegistry(registryFuncArray){
-        var registry= {pathToPackageJson:'',
+    //TODO NEEDS TESTS!
+    buildRegistry(){
+        var registry= {pathToRoot:'',
             dependencyDeclarations:[],
             renamedDeclarations:[]};
-        registryFuncArray.forEach(x=>{
+        this.registryFunkArray.forEach(x=>{
             var reg = x(new RegistryDSL());
+            registry.appRoot = registry.appRoot || reg.appRoot;
             registry.pathToPackageJson = registry.pathToPackageJson || reg.pathToPackageJson;
             if(reg.dependencyDeclarations.length>0) {
                 registry.dependencyDeclarations = registry.dependencyDeclarations.length<=0
@@ -65,7 +68,8 @@ console.log(this.registry);
 
     inject(dependencies) {
         if(!_.isArray(dependencies)){ dependencies = [dependencies];}
-        this.dependencyGraph = new Graph();
+        this.dependencyGraph = new Graph(this.registry.appRoot);
+        this.registry = this.buildRegistry();
         var packageJson =  require(this.registry.pathToPackageJson);
         this.dependencyGraph.buildGraph(packageJson);
         applyRegistry(this.registry, this.dependencyGraph);
@@ -74,8 +78,8 @@ console.log(this.registry);
             invariant(d.name, 'injected dependecy must have a name');
             invariant(d.resolvedInstance || d.path, 'injected dependency must have either a resolvedInstance or a path');
             var newDep = d.resolvedInstance
-                ? new Dependency({name:d.name, resolvedInstance:d.resolvedInstance})
-                : new Dependency({name: d.name, path: d.path, internal : d.internal});
+                ? new Dependency({name:d.name, resolvedInstance:d.resolvedInstance, appRoot:this.registry.appRoot})
+                : new Dependency({name: d.name, path: d.path, internal : d.internal, appRoot:this.registry.appRoot});
             this.dependencyGraph.addItem(newDep);
         });
         new GraphResolution().recurse(this.dependencyGraph);
